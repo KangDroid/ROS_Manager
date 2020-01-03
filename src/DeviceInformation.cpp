13244b9de368ff6afb3989c9998e187129002ffa
KangDroid <hyunwoo286@gmail.com>
2020-01-03 15:39:55 +0900

    Add some comment

--- src/DeviceInformation.cpp
+++ src/DeviceInformation.cpp
@@ -11,6 +11,14 @@ DeviceInformation::~DeviceInformation() {
     delete ui;
 }
 
+/**
+ * @brief DeviceInformation::on_thread_finish
+ * @param val when thread return string
+ *
+ * This function will set FW Information returned from thread
+ * But, when thread isn't finished, return so it wouldn't set something strange string.
+ *
+ */
 void DeviceInformation::on_thread_finish(const string val) {
     if (!(tup->isFinished())) {
         return;
@@ -22,6 +30,10 @@ void DeviceInformation::on_thread_finish(const string val) {
     ui->mFWInfoLabel->setText(str);
 }
 
+/**
+ * @brief DeviceInformation::on_mFirmwareInfo_clicked
+ * When Button clicked.
+ */
 void DeviceInformation::on_mFirmwareInfo_clicked() {
     // This SHOULD executed AFTER launching the server.
     tup = new ThreadUpdate();
